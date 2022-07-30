e20f2ee712c142d6194c27e364ae91ee35f95f38
Vergel Mañalac <verge2015@gmail.com>
2022-07-30 22:09:59 +0800

    Added the API key to .env
    Added the API key to .env
    Added .env in .gitignore
    Added the global key to movie controller

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -1,8 +1,12 @@
-const getMoviesDefault = async (val) => {
+const baseURL = "http://www.omdbapi.com";
+const apiKey = process.env.REACT_APP_API_KEY
+
+const getMoviesDefault = async (search) => {
     // const res = await fetch(`http://www.omdbapi.com/?s=spider-man&apikey=c0ad6b8f`);
     // return await res.json();
 
-    const url = `http://www.omdbapi.com/?s=${val}&p=1-10&apikey=75a7ad13`
+    // const url = `http://www.omdbapi.com/?s=${val}&p=1-10&apikey=75a7ad13`
+    const url = `${baseURL}/?s=${search}&p=1-10&apikey=${apiKey}`
     /* It fetches data from the OMDB API and sets the data to the movies variable. */
     const res = await fetch(url);
     return await res.json();
