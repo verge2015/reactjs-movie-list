8d3e9afc9027fef1aeec30755598bc4754070e69
Vergel Mañalac <verge2015@gmail.com>
2022-07-30 15:11:34 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,6 +11,7 @@ import {
 export default function Footer() {
   return (
     <Box
+      marginTop={100}
       bg={useColorModeValue('gray.50', 'gray.900')}
       color={useColorModeValue('gray.700', 'gray.200')}>
       <Container as={Stack} maxW={'6xl'} py={10}>
